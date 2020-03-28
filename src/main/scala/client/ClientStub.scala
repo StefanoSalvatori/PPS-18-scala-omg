36a2c0f0b6ecf42a590490ea41098c8f4a10a12d
Gabriele <gabriele.guerrini6@studio.unibo.it>
2020-03-28 22:14:35 +0100

    Started room filters implementation

--- src/main/scala/client/ClientStub.scala
+++ src/main/scala/client/ClientStub.scala
@@ -2,11 +2,25 @@ package client
 
 object ClientStub extends App {
 
-  private val serverAddress = "localhost"
-  private val serverPort = 8080
-  val client = Client(serverAddress, serverPort)
+  //private val serverAddress = "localhost"
+  //private val serverPort = 8080
+  //val client = Client(serverAddress, serverPort)
 
-  client createPublicRoom "test_room"
+  //client createPublicRoom "test_room"
 
   //client.shutdown()
+
+
+  import common.MyOpt
+  import common.FilterOptions
+
+  val prop1 = MyOpt[Int]("A", 3)
+  val prop2 = MyOpt[String]("B", "svv")
+  val prop3 = MyOpt[Boolean]("C", true)
+
+  val filters = (prop1 > 2) andThen (prop2 :!= "asc") andThen (prop3 := true)
+  val shortFilter = FilterOptions.just(prop1 > 2)
+
+  println(filters.options)
+  println(shortFilter.options)
 }
