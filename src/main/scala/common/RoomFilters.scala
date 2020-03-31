d89ddd36694f1d9d966af8994c2fe81f8c975a39
Gabriele <gabriele.guerrini6@studio.unibo.it>
2020-03-31 12:23:25 +0200

    Improved room properties and filters; started server filter handling logic

--- src/main/scala/common/RoomFilters.scala
+++ src/main/scala/common/RoomFilters.scala
@@ -1,32 +1,32 @@
 package common
 
-sealed trait FilterStrategy
+abstract class FilterStrategy
 case class EqualStrategy() extends FilterStrategy
 case class NotEqualStrategy() extends FilterStrategy
 case class GreaterStrategy() extends FilterStrategy
 case class LowerStrategy() extends FilterStrategy
 
-trait FilterStrategies[T] {  option: RoomProperty[T] =>
+trait FilterStrategies { option: RoomProperty =>
 
-  def =:=(that: T): FilterOption[T] = filterOption(EqualStrategy(), that)
-  def =!=(that: T): FilterOption[T] = filterOption(NotEqualStrategy(), that)
-  def >(that: T): FilterOption[T] = filterOption(GreaterStrategy(), that)
-  def <(that: T): FilterOption[T] = filterOption(LowerStrategy(), that)
+  def =:=(that: RoomPropertyValue): FilterOption = filterOption(EqualStrategy(), that)
+  def =!=(that: RoomPropertyValue): FilterOption = filterOption(NotEqualStrategy(), that)
+  def >(that: RoomPropertyValue): FilterOption= filterOption(GreaterStrategy(), that)
+  def <(that: RoomPropertyValue): FilterOption = filterOption(LowerStrategy(), that)
 
-  private def filterOption(filterStrategy: FilterStrategy, that: T): FilterOption[T] =
+  private def filterOption(filterStrategy: FilterStrategy, that: RoomPropertyValue): FilterOption =
     FilterOption(option.name, filterStrategy, that)
 }
 
-case class FilterOption[T](optName: String, strategy: FilterStrategy, value: T) {
-  def andThen(filterOpt: FilterOption[_]): FilterOptions = FilterOptions(Seq(this, filterOpt))
+case class FilterOption(optionName: String, strategy: FilterStrategy, value: RoomPropertyValue) {
+  def andThen(filterOpt: FilterOption): FilterOptions = FilterOptions(Seq(this, filterOpt))
 }
 
 object FilterOptions {
-  def just(filter: FilterOption[_]): FilterOptions = FilterOptions(Seq(filter))
+  def just(filter: FilterOption): FilterOptions = FilterOptions(Seq(filter))
   def empty(): FilterOptions = FilterOptions(Seq())
 }
 
-case class FilterOptions(options: Seq[FilterOption[_]]) {
-  def andThen(that: FilterOption[_]): FilterOptions= FilterOptions(options :+ that)
+case class FilterOptions(options: Seq[FilterOption]) {
+  def andThen(that: FilterOption): FilterOptions = FilterOptions(options :+ that)
   def ++(that: FilterOptions): FilterOptions = FilterOptions(options ++ that.options)
 }
--- src/main/scala/common/RoomProperty.scala
+++ src/main/scala/common/RoomProperty.scala
@@ -1,21 +1,29 @@
 package common
 
-case class RoomProperty[T:Ordering](name: String, value: T) extends FilterStrategies[T]
-
-object BasicRoomOptionValueComparators {
-  implicit object IntOrdering extends Ordering[Int] {
-    override def compare(x: Int, y: Int): Int = x compareTo y
-  }
-
-  implicit object StringOrdering extends Ordering[String] {
-    override def compare(x: String, y: String): Int = x compareTo y
-  }
-
-  implicit object BooleanOrdering extends Ordering[Boolean] {
-    override def compare(x: Boolean, y: Boolean): Int = x compareTo y
-  }
-}
+case class RoomProperty(name: String, value: RoomPropertyValue) extends FilterStrategies
 
 case class CustomRoomOptionValue(a: String, b: Int) extends Ordered[CustomRoomOptionValue] {
   override def compare(that: CustomRoomOptionValue): Int = this.b - that.b
+}
+
+object BasicRoomPropertyValueConversions {
+  implicit def intToIntProperty(value: Int): IntRoomPropertyValue = IntRoomPropertyValue(value)
+  implicit def stringToStringProperty(value: String): StringRoomPropertyValue = StringRoomPropertyValue(value)
+  implicit def booleanToBooleanProperty(value: Boolean): BooleanRoomPropertyValue = BooleanRoomPropertyValue(value)
+}
+
+trait RoomPropertyValue { self =>
+  def compare(that: self.type): Int
+}
+
+case class IntRoomPropertyValue(value: Int) extends RoomPropertyValue {
+  override def compare(that: this.type): Int = this.value - that.value
+}
+
+case class StringRoomPropertyValue(value: String) extends RoomPropertyValue {
+  override def compare(that: this.type): Int = this.value compareTo that.value
+}
+
+case class BooleanRoomPropertyValue(value: Boolean) extends RoomPropertyValue {
+  override def compare(that: this.type): Int = this.value compareTo that.value
 }
