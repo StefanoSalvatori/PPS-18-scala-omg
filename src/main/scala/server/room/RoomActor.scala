c01e5db297b5d2c2aa26cc502bea65c80b053f33
Stefano Salvatori <stefano.salvatori5@studio.unibo.it>
2020-04-09 19:17:34 +0200

    Fix text on room actor

--- src/main/scala/server/room/RoomActor.scala
+++ src/main/scala/server/room/RoomActor.scala
@@ -43,13 +43,16 @@ class RoomActor(private val serverRoom: ServerRoom) extends Actor with ActorLogg
     // case NotifyAll(msg) => this.serverRoom.broadcast(msg)
     case Join(client) =>
       this.serverRoom.addClient(client) //TODO: add checks if user can join
+      sender ! JoinOk
     case Leave(client) =>
       this.serverRoom.removeClient(client)
+      sender ! ClientLeaved
     case Msg(client, payload) =>
       if (this.serverRoom.clientAuthorized(client)) {
         this.serverRoom.onMessageReceived(client, payload)
       } else {
         client.send(ClientNotAuthorized)
+        sender ! ClientNotAuthorized
       }
   }
 
