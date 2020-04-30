e740045c9f0aeb7a0231fac4d2bef2531e539dca
Stefano Salvatori <stefano.salvatori5@studio.unibo.it>
2020-04-30 17:38:56 +0200

    Removed examples

--- src/main/scala/examples/roll_the_dice/common/ClientInfo.scala
+++
@@ -1,3 +0,0 @@
-package examples.roll_the_dice.common
-
-case class ClientInfo(desiredTeam: Team)
--- src/main/scala/examples/roll_the_dice/common/MatchState.scala
+++
@@ -1,9 +0,0 @@
-package examples.roll_the_dice.common
-
-@SerialVersionUID(1234L) // scalastyle:ignore magic.number
-case class MatchState(pointsA: Int = 0, pointsB: Int = 0) extends java.io.Serializable {
-
-  def addPointsA(points: Int): MatchState = MatchState(pointsA + points, pointsB)
-
-  def addPointsB(points: Int): MatchState = MatchState(pointsA, pointsB + points)
-}
